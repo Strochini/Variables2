c1be23cab90e9f3ddc70c6a52f894120c43ca3bf
Strochini <0.618@bk.ru>
2022-11-10 01:14:04 +0300

    Variables2 vidLes2Variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        int bananas = 200;
+        System.out.println("Бананов " + bananas + " кг");
+
+
+        float sugar = 3;
+        float onePortion = sugar / 4;
+        System.out.println("Одна порция весит " + onePortion + " кг");
+
+
+        byte a = 1;
+        short b = 1;
+        int c = 1;
+        int d = a + b + c;
+        System.out.println(d);
+
+        float g = a + 1f;
+        System.out.println(g);
     }
 }
